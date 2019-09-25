32748b27b1682720731473ab035caebabbfe88bf
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-09-25 23:56:26 +0530

    added fragment transition in home activity

---
+++ app/src/main/java/com/rishabh/concetto2019/HomePage/MVP/ScheduleFragment2.java
@@ -0,0 +1,32 @@
+package com.rishabh.concetto2019.HomePage.MVP;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rishabh.concetto2019.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScheduleFragment2 extends Fragment {
+
+
+    public ScheduleFragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule_fragment2, container, false);
+    }
+
+}
